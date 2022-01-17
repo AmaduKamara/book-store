bebcfeb1989a1a8fa189c30ae4bd0aa27df613eb
Amkam <amaduskamara@gmail.com>
2022-01-17 17:11:22 +0000

    Add basic styles for stylelint sake

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import { Outlet } from 'react-router-dom';
-
+import './inxed.css';
 import Books from './pages/Books';
 
 function App() {
   return (
-    <>
+    <div className="container">
       <Books />
       <Outlet />
-    </>
+    </div>
   );
 }
 
--- src/pages/Categories.js
+++ src/pages/Categories.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import '../inxed.css';
 
 function Categories() {
   return (
-    <div>
+    <div className="container">
       <h1>Under construction</h1>
     </div>
   );
