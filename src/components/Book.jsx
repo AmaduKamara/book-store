bd7a07a0dbeb2c3e0ddbc8a5c2285973cf505501
Amkam <amaduskamara@gmail.com>
2022-01-18 07:57:57 +0000

    Add author field and category and styled them

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -4,7 +4,22 @@ import PropTypes from 'prop-types';
 function Book({ title }) {
   return (
     <div className="my-5 p-6 border flex justify-between items-center">
-      <h2 className="text-xl font-semibold">{title}</h2>
+      <div>
+        <p className="text-gray-400 text-xl mb-1">Action</p>
+        <h2 className="text-2xl font-semibold">{title}</h2>
+        <h3 className="text-xl ">Author</h3>
+        <div>
+          <button type="button" className="mr-2 mt-2 text-sky-300 text-xl">
+            Comment
+          </button>
+          <button type="button" className="mx-2 mt-2 text-sky-300 text-xl">
+            Remove
+          </button>
+          <button type="button" className="mx-2 mt-2 text-sky-300 text-xl">
+            Edit
+          </button>
+        </div>
+      </div>
       <button
         type="button"
         className="py-3 px-16 rounded border border-red-400 hover:border-red-300 hover:bg-red-400 hover:text-white"
--- src/components/BookForm.jsx
+++ src/components/BookForm.jsx
@@ -7,14 +7,25 @@ function BookForm() {
       <h3 className="text-2xl uppercase font-semibold text-gray-400 my-3">
         Add New Book
       </h3>
-      <div>
-        <input
-          type="text"
-          placeholder="Book title"
-          className="p-3 border rounded w-3/5 focus:outline-none"
-          required
-        />
-        <select name="category" className="p-3 border mx-4  w-1/5">
+      <div className="flex">
+        <div className="w-3/5">
+          <input
+            type="text"
+            placeholder="Book title"
+            className="p-3 border rounded focus:outline-none border-sky-500 w-2/3 mr-4"
+            required
+          />
+          <input
+            type="text"
+            placeholder="Author"
+            className="p-3 border rounded focus:outline-none border-sky-500"
+            required
+          />
+        </div>
+        <select
+          name="category"
+          className="p-3 border mr-4  w-1/5 border-sky-500 rounded"
+        >
           <option value="" className="my-2">
             Category
           </option>
